eda014257042e7a6c4c76f813496987544675f0b
Saull Brandão <saull@outlook.com>
2022-11-04 09:46:32 -0300

    Criando serviço manualmente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursosService } from './cursos/cursos.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, CursosComponent],
+  imports: [BrowserModule],
+  providers: [CursosService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/cursos/cursos.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CursosService } from './cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css'],
+})
+export class CursosComponent implements OnInit {
+  cursos: string[] = [];
+
+  constructor(private cursosService: CursosService) {}
+
+  ngOnInit(): void {
+    this.cursos = this.cursosService.getCursos();
+  }
+}
